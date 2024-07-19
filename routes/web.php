8fc8783e80a3907a9667b6d510d493f03fbca924
Malik N <malik@tti.care>
2024-07-18 22:41:10 -0700

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileUploader;
+use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/login', function() {
+    return view('login');
+})->name('login');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::group(['middleware' => ['auth']], function() {
+
+    Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+    Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+
+    Route::post('/movies/{id}', [ MovieController::class, 'update' ]);
+
+    Route::post('/upload', [ FileUploader::class, 'upload' ])->middleware(['auth']);
+//    Route::get('/api/movies', [ MovieController::class, 'index' ])->middleware(['auth']);
+//    Route::get('/api//movies/{id}', [ MovieController::class, 'show' ]);
+    Route::post('/movies/delete/{id}', [ MovieController::class, 'delete' ]);
+
 });
